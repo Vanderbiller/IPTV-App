13b339ac97b41fcf2a03c342fd19fa4226dc51a6
Adam Vanbaelinghem <adam.vanbaelinghem@ucdenver.edu>
2024-11-16 00:22:20 -0700

    updates: Moved from a simple list of channels to a hashmap, provides a better time efficiency for grouping channels together based off their metadata. Fixed channels not being loaded into the hashmap if they lack metadata, now all m3u syntax should be working and registered into the channels map. Added an official return for the function, now when called will return the hashmap of all the channels parsed from the m3u file. Formatted file to match dart syntax formatting

--- lib/m3uparser.dart
+++ lib/m3uparser.dart
@@ -8,55 +8,57 @@ class Channel {
   final String url;
   final String grouping;
 
-  Channel({
-    required this.name,
-    required this.logo,
-    required this.url,
-    required this.grouping
-  });
+  Channel(
+      {required this.name,
+      required this.logo,
+      required this.url,
+      required this.grouping});
 }
 
 class M3UParser {
-  Future<List<Channel>> parseM3U(String url) async {
+  Future<Map<String?, List<Channel>>> parseM3U(String url) async {
     final client = http.Client();
     final request = http.Request('GET', Uri.parse(url));
     final res = await client.send(request);
 
     if (res.statusCode == 200) {
-      List<Channel> channels = [];
+      Map<String?, List<Channel>> channels = {};
 
-      final stream = res.stream
-        .transform(utf8.decoder)
-        .transform(const LineSplitter());
+      final stream =
+          res.stream.transform(utf8.decoder).transform(const LineSplitter());
 
       String? currentName;
       String? currentLogo;
       String? currentGrouping;
 
-      print("Started");
       await for (var line in stream) {
         if (line.startsWith('#EXTINF')) {
-          currentName = RegExp(r'tvg-name="([^"]*)"').firstMatch(line)?.group(1);
-          currentLogo = RegExp(r'tvg-logo="([^"]*)"').firstMatch(line)?.group(1);
-          currentGrouping = RegExp(r'group-title="([^"]*)"').firstMatch(line)?.group(1);
+          currentName =
+              RegExp(r'tvg-name="([^"]*)"').firstMatch(line)?.group(1);
+          currentName ??= line.split(',').length > 1
+              ? line.split(',')[1].trim()
+              : ''; //Condition if tvg-name isnt present
+          currentLogo =
+              RegExp(r'tvg-logo="([^"]*)"').firstMatch(line)?.group(1);
+          currentLogo ??= '';
+
+          currentGrouping =
+              RegExp(r'group-title="([^"]*)"').firstMatch(line)?.group(1);
+          currentGrouping ??= 'Other';
         } else if (line.startsWith('http') && currentName != null) {
-          channels.add(Channel(
-            name: currentName,
-            logo: currentLogo ?? '',
-            url: line.trim(),
-            grouping: currentGrouping ?? ''
-          ));
+          if (!channels.containsKey(currentGrouping)) {
+            channels[currentGrouping] = [];
+          }
+          channels[currentGrouping]!.add(Channel(
+              name: currentName,
+              logo: currentLogo ?? '',
+              url: line.trim(),
+              grouping: currentGrouping ?? ''));
         }
       }
-      print("finished");
-      channels;
-    }
-    
-    else {
+      return channels;
+    } else {
       throw Exception("Failed to load M3U File");
     }
-    
-
-    return List<Channel>.empty();
   }
 }
