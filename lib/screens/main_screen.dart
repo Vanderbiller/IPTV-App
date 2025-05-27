0d5e1ea3ed5a3c59ef5abf0d2140203d163657a9
Vanderbiller <adam.vanbaelinghem@outlook.com>
2025-05-27 16:33:11 -0600

    changes in main_screen: removed the dynamic bottom app bar and made it static. Added shows button to the app bar and appropriate navigation to it.

--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:sample_app/models/channel.dart';
 import 'package:sample_app/models/movie.dart';
+import 'package:sample_app/models/show.dart';
 import '../managers/m3u_manager.dart';
 import 'channel_groups_screen.dart';
 import 'movies_screen.dart';
+import 'show_screen.dart';
 
 class MainScreen extends StatefulWidget {
   final String m3uUrl;
@@ -17,37 +19,18 @@ class MainScreen extends StatefulWidget {
   State<MainScreen> createState() => _MainScreenState();
 }
 
-class _MainScreenState extends State<MainScreen> with SingleTickerProviderStateMixin {
+class _MainScreenState extends State<MainScreen> {
   int _selectedIndex = 0;
   Map<String?, List<Channel>>? _channelsByGroup;
   Map<String?, List<Movie>>? _moviesByCategory;
+  Map<String, Show>? _showsByTitle;
   bool _isLoading = true;
   String? _errorMessage;
-  bool _showBottomBar = true;
-  double _lastScrollPosition = 0;
-  late AnimationController _animationController;
-  late Animation<double> _animation;
 
   @override
   void initState() {
     super.initState();
     _loadContent();
-    _animationController = AnimationController(
-      vsync: this,
-      duration: const Duration(milliseconds: 300),
-    );
-    _animation = Tween<double>(begin: 0, end: 1).animate(
-      CurvedAnimation(
-        parent: _animationController,
-        curve: Curves.easeInOut,
-      ),
-    );
-  }
-
-  @override
-  void dispose() {
-    _animationController.dispose();
-    super.dispose();
   }
 
   Future<void> _loadContent() async {
@@ -63,16 +46,9 @@ class _MainScreenState extends State<MainScreen> with SingleTickerProviderStateM
       setState(() {
         _channelsByGroup = result.channelsByGroup;
         _moviesByCategory = result.moviesByCategory;
+        _showsByTitle = result.showsByTitle;
         _isLoading = false;
       });
-      // Show bottom bar if there are movies
-      final hasMovies = result.moviesByCategory.isNotEmpty;
-      if (hasMovies) {
-        setState(() {
-          _showBottomBar = true;
-        });
-        _animationController.forward();
-      }
     } catch (e) {
       if (!mounted) return;
       setState(() {
@@ -80,39 +56,6 @@ class _MainScreenState extends State<MainScreen> with SingleTickerProviderStateM
         _errorMessage = 'Failed to load content. Please check the URL.';
       });
     }
-  }
-
-  bool _handleScroll(ScrollNotification notification) {
-    if (notification is ScrollUpdateNotification) {
-      final currentScroll = notification.metrics.pixels;
-      
-      // Show bottom bar if at the top of the page
-      if (currentScroll <= 0) {
-        if (!_showBottomBar) {
-          setState(() {
-            _showBottomBar = true;
-          });
-          _animationController.forward();
-        }
-      } else {
-        // Handle scroll direction
-        if (currentScroll > _lastScrollPosition && _showBottomBar) {
-          // Scrolling down
-          setState(() {
-            _showBottomBar = false;
-          });
-          _animationController.reverse();
-        } else if (currentScroll < _lastScrollPosition && !_showBottomBar) {
-          // Scrolling up
-          setState(() {
-            _showBottomBar = true;
-          });
-          _animationController.forward();
-        }
-      }
-      _lastScrollPosition = currentScroll;
-    }
-    return true;
   }
 
   @override
@@ -141,75 +84,66 @@ class _MainScreenState extends State<MainScreen> with SingleTickerProviderStateM
     }
 
     final hasMovies = _moviesByCategory != null && _moviesByCategory!.isNotEmpty;
+    final hasShows = _showsByTitle != null && _showsByTitle!.isNotEmpty;
 
     return Scaffold(
       backgroundColor: Colors.black,
-      body: Stack(
-        children: [
-          NotificationListener<ScrollNotification>(
-            onNotification: _handleScroll,
-            child: IndexedStack(
-              index: _selectedIndex,
-              children: [
-                ChannelGroupsScreen(
-                  channels: _channelsByGroup,
+      bottomNavigationBar: (hasMovies || hasShows)
+          ? Container(
+              decoration: const BoxDecoration(
+                color: Colors.black,
+                border: Border(
+                  top: BorderSide(
+                    color: Colors.white10,
+                    width: 0.5,
+                  ),
                 ),
-                if (hasMovies) MoviesScreen(movies: _moviesByCategory),
-              ],
-            ),
-          ),
-          if (hasMovies)
-            Positioned(
-              left: 0,
-              right: 0,
-              bottom: 0,
-              child: AnimatedBuilder(
-                animation: _animation,
-                builder: (context, child) {
-                  return Transform.translate(
-                    offset: Offset(0, 100 * (1 - _animation.value)),
-                    child: Opacity(
-                      opacity: _animation.value,
-                      child: Container(
-                        decoration: BoxDecoration(
-                          color: Colors.black.withOpacity(0.8),
-                          boxShadow: [
-                            BoxShadow(
-                              color: Colors.black.withOpacity(0.3),
-                              blurRadius: 8,
-                              offset: const Offset(0, -2),
-                            ),
-                          ],
-                        ),
-                        child: BottomNavigationBar(
-                          currentIndex: _selectedIndex,
-                          onTap: (index) {
-                            setState(() {
-                              _selectedIndex = index;
-                            });
-                          },
-                          backgroundColor: Colors.transparent,
-                          elevation: 0,
-                          selectedItemColor: Colors.redAccent,
-                          unselectedItemColor: Colors.white70,
-                          items: const [
-                            BottomNavigationBarItem(
-                              icon: Icon(Icons.tv),
-                              label: 'Channels',
-                            ),
-                            BottomNavigationBarItem(
-                              icon: Icon(Icons.movie),
-                              label: 'Movies',
-                            ),
-                          ],
-                        ),
-                      ),
-                    ),
-                  );
-                },
               ),
+              child: BottomNavigationBar(
+                currentIndex: _selectedIndex,
+                onTap: (index) {
+                  setState(() {
+                    _selectedIndex = index;
+                  });
+                },
+                backgroundColor: Colors.black,
+                elevation: 0,
+                selectedItemColor: Colors.redAccent,
+                unselectedItemColor: Colors.white70,
+                items: [
+                  const BottomNavigationBarItem(
+                    icon: Icon(Icons.tv_rounded),
+                    activeIcon: Icon(Icons.tv_rounded, color: Colors.redAccent),
+                    label: 'Channels',
+                  ),
+                  if (hasMovies)
+                    const BottomNavigationBarItem(
+                      icon: Icon(Icons.movie_creation_rounded),
+                      activeIcon: Icon(Icons.movie_creation_rounded, color: Colors.redAccent),
+                      label: 'Movies',
+                    ),
+                  if (hasShows)
+                    const BottomNavigationBarItem(
+                      icon: Icon(Icons.play_circle_outline_rounded),
+                      activeIcon: Icon(Icons.play_circle_outline_rounded, color: Colors.redAccent),
+                      label: 'Shows',
+                    ),
+                ],
+              ),
+            )
+          : null,
+      body: SafeArea(
+        bottom: false,
+        child: IndexedStack(
+          index: _selectedIndex,
+          children: [
+            ChannelGroupsScreen(
+              channels: _channelsByGroup,
             ),
-        ],
+            if (hasMovies) MoviesScreen(movies: _moviesByCategory),
+            if (hasShows) ShowScreen(shows: _showsByTitle),
+          ],
+        ),
       ),
     );
   }
